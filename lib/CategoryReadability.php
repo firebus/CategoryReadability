b97fc99b6bfc7c7abac27cc7e12e574663be50fc
firebus <russ@firebus.com>
2016-06-29 22:55:01 -0700

    CategoryReadability::getExtract()

--- lib/CategoryReadability.php
+++ lib/CategoryReadability.php
@@ -3,14 +3,11 @@
 namespace Firebus;
 
 /**
- * Procedural logic for category-readability here. Based on the parameters passed in, we're going to:
- * - Query a mediawiki API endpoint for the list of pages in a category
- * - Get a parse representation of each page
- * - Pull out the first paragraph from that parsed representation
- * - Score the first paragraph for readability using the Flesch-Kincaid Reading Ease algorithm from https://github.com/DaveChild/Text-Statistics
- * - Send an array of pages to Output
+ * Turn the requested category into a list of articles sorted by readability
  */
 class CategoryReadability {
+	
+	const CMLIMIT = 5;
 
 	/** @var string $apiUrl without protocol we're going to assume https:// */
 	private $apiUrl;
@@ -26,29 +23,51 @@ class CategoryReadability {
 		$this->category = $category;
 		$this->output = $output;
 	}
-	
+
+	/**
+	 * Procedural logic goes here.
+ 	 * # Query the mediawiki categorymembers API for the list of pages in a category
+	 * # Query the mediawiki extracts API for an extract from each page
+	 * # Score the extract for readability using the Flesch-Kincaid Reading Ease algorithm via https://github.com/DaveChild/Text-Statistics
+	 * # Sort the pages by score
+	 * # Send the pages to Output
+	 */
 	public function execute() {
-		$scoredPages = array();
 		$pages = $this->getPagesForCategory();
 		foreach ($pages as &$page) {
 			$extract = $this->getExtract($page->pageid);
 			$page->score = $this->scoreText($extract);
 		}
-		
-		$this->output->articleList($pages);
+
+		$sortedPages = $this->sortPages($pages);
+		$this->output->articleList($sortedPages);
 	}
 	
 	/**
-	 * Make an API call to Categorymembers
-	 * @todo sanity apiUrl and category
-	 * @return array of pages
+	 * Make an API call to categorymembers
+	 * @todo sanitize apiUrl and category
+	 * @return array of page object
 	 */
 	private function getPagesForCategory() {
-		$url = "https://{$this->apiUrl}?action=query&list=categorymembers&cmtitle=Category:{$this->category}&cmlimit=50&cmtype=page&format=json";		
+		$url = "https://{$this->apiUrl}?action=query&list=categorymembers&cmtitle=Category:{$this->category}"
+			. "&cmlimit=" . self::CMLIMIT . "&cmtype=page&format=json";
 		$pages = json_decode(file_get_contents($url));
 		return $pages->query->categorymembers;
 	}
 	
-	private function getExtract($pageId) {}
+	/**
+	 * Make an API call to extracts
+	 * @note I tried setting multiple pageids and setting exlimit=max, but MW refused to give me more than one extract
+	 * @param string $pageid
+	 * @return string
+	 */
+	private function getExtract($pageid) {
+		$url = "https://{$this->apiUrl}?action=query&prop=extracts&exsentences=10&explaintext&pageids=$pageid"
+			. "&format=json";
+		$extracts = json_decode(file_get_contents($url));
+		return $extracts->query->pages->$pageid->extract;
+	}
+	
 	private function scoreText($text) {}
+	private function sortPages($pages) {}
 }
